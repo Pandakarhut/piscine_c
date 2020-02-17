c72e57485eccdd59cf7068ed06de5ef414feaef6
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-17 23:59:01 +0200

    Update purpur.c

--- 01/1-2-first_word/purpur.c
+++ 01/1-2-first_word/purpur.c
@@ -1,17 +1,12 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
-{
-  write(1, &c, 1);
-}
-
 int main(int argc, char **argv)
 {
   if (argc == 2)
   {
     while(*argv[1] != ' ' && *argv[1] != '\t' && *argv[1])
-      ft_putchar(*argv[1]++);
+      write(1, argv[1]++, 1);
   }
-  ft_putchar('\n');
+  write(1, "\n", 1);
   return(0);
 }
