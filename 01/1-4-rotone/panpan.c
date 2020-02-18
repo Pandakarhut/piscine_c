7d54e66f9f9dda07d887f96f9ce9bf798956ed45
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-18 23:40:56 +0200

    Create panpan.c

---
+++ 01/1-4-rotone/panpan.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+int rot_13(char c)
+{
+  if ((c >= 'A' && c <= 'Y') || (c >= 'a' && c <= 'y'))
+    c += 1;
+  else if ((c == 'z') || (c == 'Z'))
+    c -= 25;
+  return (c);
+}
+
+int main(int ac, char **av)
+{
+  if (ac == 2)
+    while (*av[1])
+      ft_putchar(rot_13(*av[1]++));
+    ft_putchar('\n');
+  return (0);
+}
