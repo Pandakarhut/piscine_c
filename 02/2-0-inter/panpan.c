059b117715de027b7df7e05e074975540529ca25
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-20 01:59:59 +0200

    Create panpan.c

---
+++ 02/2-0-inter/panpan.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+  int characters[256] = {};
+  int i;
+  int j;
+  i = 0;
+  j = 0;
+
+  while (argc == 3 && argv[1][i])
+  {
+    while (argv[2][j])
+    {
+      if (argv[1][i] == argv[2][j] && characters[1][i] != 1)
+      {
+        write(1, &(argv[1][i]), 1);
+        characters[argv[1][i]] = 1;
+      }
+      j++;
+    }
+    j = 0;
+    i++;
+  }
+  write(1, "\n", 1);
+  return (0);
+}
