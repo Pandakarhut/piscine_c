ba1603e82dd2bb3896db7cbd208ff17185e3e828
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-20 23:00:42 +0200

    Update panpan.c

--- 02/2-4-do_op/panpan.c
+++ 02/2-4-do_op/panpan.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int		main(int argc, char **argv)
+int		main(int ac, char **av)
 {
-	if	(argc == 4)
+	if	(ac == 4)
 	{
-		if (argv[2][0] == '+')
-			printf("%d", (atoi(argv[1]) + atoi(argv[3])));
-		if (argv[2][0] == '-')
-			printf("%d", (atoi(argv[1]) - atoi(argv[3])));
-		if (argv[2][0] == '*')
-			printf("%d", (atoi(argv[1]) * atoi(argv[3])));
-		if (argv[2][0] == '/')
-			printf("%d", (atoi(argv[1]) / atoi(argv[3])));
-		if (argv[2][0] == '%')
-			printf("%d", (atoi(argv[1]) % atoi(argv[3])));
+		if (av[2][0] == '+')
+			printf("%d", (atoi(av[1]) + atoi(av[3])));
+		if (av[2][0] == '-')
+			printf("%d", (atoi(av[1]) - atoi(av[3])));
+		if (av[2][0] == '*')
+			printf("%d", (atoi(av[1]) * atoi(av[3])));
+		if (av[2][0] == '/')
+			printf("%d", (atoi(av[1]) / atoi(av[3])));
+		if (av[2][0] == '%')
+			printf("%d", (atoi(av[1]) % atoi(av[3])));
 	}
 	printf("\n");
 	return (0);
