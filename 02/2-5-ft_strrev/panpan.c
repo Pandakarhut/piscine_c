adf70e5a57f3bbc08f788f351e9d502a14a5abdc
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-21 13:07:05 +0200

    Create panpan.c

---
+++ 02/2-5-ft_strrev/panpan.c
@@ -0,0 +1,21 @@
+char *ft_strrev(char *s)
+{
+  int i;
+  int j;
+  char tmp;
+  
+  i = 0;
+  while (s[i])
+    i++;
+  i--;
+  j = 0;
+  while (j < i)
+  {
+    tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+    i--;
+    j++;
+  }
+  return (s);
+}
