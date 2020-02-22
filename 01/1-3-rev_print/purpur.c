6d07b1efebedce309d8b6df663e08a74ded0381c
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-23 01:31:04 +0200

    Update and rename purpur.txt to purpur.c

--- 01/1-3-rev_print/purpur.c
+++ 01/1-3-rev_print/purpur.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 { 
   int i;
   i = 0;
-  if (argc == 2)
+  if (ac == 2)
   {
-    while (argv[1][i])
+    while (av[1][i])
       i++;
   
     i--;
     while (i >= 0)
-      write(1, &argv[1][i--], 1);
+      write(1, &av[1][i--], 1);
   }
   write(1, "\n", 1);
   return (0);
