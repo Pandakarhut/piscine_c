66018c25b01d34556c36e1c39710dcd318028939
JJ <57943987+Pandakarhut@users.noreply.github.com>
2020-02-22 21:53:24 +0200

    Update panpan.c

--- 02/2-0-inter/panpan.c
+++ 02/2-0-inter/panpan.c
@@ -12,10 +12,10 @@ int main(int argc, char **argv)
   {
     while (argv[2][j])
     {
-      if (argv[1][i] == argv[2][j] && characters[argv[1][i]] != 1)
+      if (argv[1][i] == argv[2][j] && characters[(int)argv[1][i]] != 1)
       {
         write(1, &(argv[1][i]), 1);
-        characters[argv[1][i]] = 1;
+        characters[(int)argv[1][i]] = 1;
       }
       j++;
     }
