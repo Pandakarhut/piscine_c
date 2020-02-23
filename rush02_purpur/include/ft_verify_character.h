395780adf2f8fa0a385b5dd88cf8a3349d54a06e
Joonas Köppä <39054050+JKoppa@users.noreply.github.com>
2020-02-23 02:35:21 +0200

    Add files via upload

---
+++ rush02_purpur/include/ft_verify_character.h
@@ -0,0 +1,4 @@
+#ifndef FT_VERIFY_CHARACTER_H
+# define FT_VERIFY_CHARACTER_H
+char *verify_character(char c, char *input);
+#endif
---
+++ rush02_purpur/include/rushes.h
@@ -0,0 +1,5 @@
+#ifndef RUSHES_H
+# define RUSHES_H
+int match_rush_00(char *input, int rows, int columns);
+int match_rush_01(char *input, int rows, int columns);
+#endif
