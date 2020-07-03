da22a26f9678147cafc2dc6f390cae850c093aa8
Jing Tian <jingtian@Jings-MacBook-Pro.local>
2020-07-03 17:23:46 +0300

    arrange and merge

--- rush02/rush00/rush00/main.c
+++ rush02/rush00/rush00/main.c
@@ -9,32 +9,11 @@
 /*   Updated: 2020/02/09 16:35:01 by mamesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 void	ft_rush(int x, int y);
 
-int	ft_atoi(const char *str)
+int		main(void)
 {
-	int	res;
-	int	negative;
-
-	negative = 1;
-	res = 0;
-	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
-			*str == '\v' || *str == '\f' || *str == '\r'))
-		++str;
-	if (*str == '-')
-		negative = -1;
-	if (*str == '-' || *str == '+')
-		++str;
-	while (*str && *str >= '0' && *str <= '9')
-	{
-		res = res * 10 + (*str - 48);
-		++str;
-	}
-	return (res * negative);
-}
-
-int		main(int ac, char **av)
-{
-	ft_rush(ft_atoi(av[1]), ft_atoi(av[2]));
+	ft_rush(5, 5);
 	return (0);
 }
